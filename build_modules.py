8ae527bb75fe5495f3b11cd27523b81417729d2a
Donny Lawrence <trea@treamous.com>
2017-06-03 20:21:04 -0500

    Update build scripts for new system.

--- build_modules.py
+++ build_modules.py
@@ -1,38 +1,31 @@
 import os, sys, subprocess, shutil
-
-def check_for_module_builder():
-    if os.path.exists("voxel_native/scripts/"):
-        return
-
-    print("Downloading P3DModuleBuilder...")
-
-    cmd = [sys.executable, "-B", "voxel_native/download_P3DModuleBuilder.py"]
-    try:
-        output = subprocess.check_output(cmd, stderr=sys.stderr)
-    except subprocess.CalledProcessError as errorMsg:
-        print(errorMsg)
-        print("Couldn't download P3DModuleBuilder.")
-        sys.exit(-1)
+import argparse
 
 def build_modules():
     print("Building native modules...")
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--debug", action="store_true", help="Builds a debug module")
+    parser.add_argument(
+        "--panda-path", type=str, help="Where the Panda3D libraries are located")
+    args = parser.parse_args()
 
     os.chdir(os.path.split(os.path.realpath(__file__))[0])
-    check_for_module_builder()
-
-    cmd = [sys.executable, "-B", "-m", "voxel_native.build"]
+    
+    cmd = [sys.executable, "-B", "-m", "P3DModuleBuilder.build", "--source-dir", "../native", "--output-dir", "../"]
+    
+    if args.debug:
+        cmd += ["--optimize", "1"]
+    if args.panda_path:
+        cmd += ["--panda-path", args.panda_path]
+    
     try:
         output = subprocess.run(cmd, stderr=sys.stderr, stdout=sys.stdout, check=True)
     except subprocess.CalledProcessError as errorMsg:
         print(errorMsg)
         print("Error building the native modules.")
         sys.exit(-1)
-
-    from voxel_native.scripts.common import is_macos, is_windows, is_linux
-    if is_windows():
-        shutil.move("voxel_native/voxel_native.pyd", "voxel/voxel_native.pyd")
-    elif is_macos() or is_linux():
-        shutil.move("voxel_native/voxel_native.so", "voxel/voxel_native.so")
 
 if __name__ == "__main__":
     build_modules()
